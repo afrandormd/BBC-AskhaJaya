6a86644a3796d73f31cadea5966a32a5145f2bd1
Afrando Sharein Ramadhan <afrandormd@gmail.com>
2025-01-05 21:52:21 +0700

    rando: membuat section products pilihan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import HeroSection from "@/components/fragments/HeroSection";
+import Products from "@/components/fragments/ProductSection";
 
 export default function Home() {
   return (
     <>
       {/* Section Hero */}
       <HeroSection />
+
+      {/* Products Section */}
+      <Products />
     </>
   );
 }
